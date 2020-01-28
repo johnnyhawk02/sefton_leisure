5a6162e9f15e5d88291285509894321ec0df2d08
johnny <johnnyhawk@gmail.com>
2020-01-28 00:08:08 +0000

    Initial commit

---
+++ lib/event.dart
@@ -0,0 +1,263 @@
+import 'events_list.dart';
+import 'package:flutter/material.dart';
+
+double convertStringTimeToDouble(s) {
+  return double.parse(s.split(':')[0]) + double.parse(s.split(':')[1]) / 60;
+}
+
+Map dayColors = {
+  'Monday': Colors.orange,
+  'Tuesday': Colors.redAccent,
+  'Wednesday': Colors.purpleAccent,
+  'Thursday': Colors.cyan,
+  'Friday': Colors.green,
+  'Saturday': Colors.teal,
+  'Sunday': Colors.redAccent,
+};
+
+Map<String, double> daysIndex = {
+  'Monday': 0,
+  'Tuesday': 1,
+  'Wednesday': 2,
+  'Thursday': 3,
+  'Friday': 4,
+  'Saturday': 5,
+  'Sunday': 6,
+};
+
+abstract class TimeTable {
+  static String closing = '22:00';
+  static String opening = '06:30';
+
+  //Times();
+  static double get left => convertStringTimeToDouble(opening);
+  static double get right => convertStringTimeToDouble(closing);
+  static double get yOffset => 100;
+  static double get multiplier => 1 / (right - left);
+}
+
+class Event {
+  String site, name, type, info, day, start, finish;
+
+  Event({
+    this.site,
+    this.name,
+    this.type,
+    this.info,
+    this.day,
+    this.start,
+    this.finish,
+  });
+
+  double get left =>
+      (convertStringTimeToDouble(start) - TimeTable.left) *
+          TimeTable.multiplier;
+  double get right =>
+      (convertStringTimeToDouble(finish) - TimeTable.left) *
+          TimeTable.multiplier;
+  double get width => right - left;
+  double get height => 1 / 14;
+  double get dayIndex => daysIndex[day];
+  String get poolType {
+    if (this.info.contains('Learner')) {
+      return 'learner';
+    } else if (this.info.contains('Main')) {
+      return 'main';
+    } else {
+      return null;
+    }
+  }
+
+  double get top =>
+      (poolType == 'learner' ? daysIndex[day] + 0.5 : daysIndex[day] + 0.0) / 7;
+
+  String get shortName =>
+      this.name.replaceAll('Les Mills ', '').replaceAll(' Virtual', '');
+
+  bool get isVirtual => name.toLowerCase().contains('virtual');
+
+  Color get color {
+    String tmp = this.name.toLowerCase();
+    if (tmp.contains('public swim')) {
+      return Colors.blue;
+    }
+    if (tmp.contains('bootle and north')) {
+      return Colors.green;
+    }
+    if (tmp.contains('adults only')) {
+      return Colors.cyan;
+    }
+    if (tmp.contains('swim & splash')) {
+      return Colors.pinkAccent;
+    }
+
+    if (tmp.contains('pump')) {
+      return Colors.red;
+    }
+    if (tmp.contains('spin') ||
+        tmp.contains('trip') ||
+        tmp.contains('sprint') ||
+        tmp.contains('rpm')) {
+      return Colors.blue;
+    }
+    if (tmp.contains('yoga')) {
+      return Colors.yellow;
+    }
+    if (tmp.contains('pilates')) {
+      return Colors.green;
+    }
+    if (tmp.contains('zumba')) {
+      return Colors.green;
+    }
+    if (tmp.contains('aqua')) {
+      return Colors.pink;
+    }
+    if (tmp.contains('combat')) {
+      return Colors.teal;
+    }
+    if (tmp.contains('tai chi')) {
+      return Colors.indigoAccent;
+    }
+
+    if (tmp.contains('grit') || tmp.contains('hiit')) {
+      return Colors.deepOrangeAccent;
+    }
+
+    if (tmp.contains('padwork') || tmp.contains('boxer')) {
+      return Colors.purple;
+    }
+    return Colors.grey;
+  }
+
+  Color get dayColor {
+    print(this.day);
+    return dayColors[this.day];
+  }
+}
+
+class LeisureCentre {
+  List classFilters = [
+    'all',
+    'pump',
+    'bike',
+    'yoga',
+    'zumba',
+    'boxer',
+    'cx',
+    'grit',
+    'hiit',
+    'public swim',
+    'adult swim',
+    'public swim learner pool'
+  ];
+
+  List days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ];
+  List<Event> events = [];
+  String openingTime;
+  String closingTime;
+  LeisureCentre({
+    this.openingTime,
+    this.closingTime,
+  }) {
+    eventsList.forEach((e) {
+      events.add(Event(
+        site: e['site'],
+        name: e['name'],
+        type: e['type'],
+        info: e['info'],
+        day: e['day'],
+        start: e['time'].split(' - ')[0],
+        finish: e['time'].split(' - ')[1],
+      ));
+    });
+  }
+  List virtualEventList() => events.where((e) => e.isVirtual).toList();
+
+  List classList({String filterDay, String className}) {
+    List<Event> tmpEvents = [];
+    List<Event> filteredEvents = [];
+    if (filterDay == null || filterDay == '') {
+      tmpEvents = events;
+
+    } else {
+      tmpEvents = events
+          .where((e) => e.day.toLowerCase() == filterDay.toLowerCase())
+          .toList();
+    }
+
+    switch (className) {
+      case 'all':
+        {
+          filteredEvents = tmpEvents.where((e) => e.type == 'class').toList();
+        }
+        break;
+
+      case 'pool':
+        {
+          filteredEvents = tmpEvents.where((e) => e.type == 'pool').toList();
+        }
+        break;
+
+      case 'public swim':
+        {
+          tmpEvents.forEach((e) {
+            if ((e.name == 'Adults Only' || e.name == 'Public Swim') &&
+                e.poolType == 'main') {
+              filteredEvents.add(e);
+            }
+          });
+        }
+        break;
+
+      case 'adult swim':
+        {
+          tmpEvents.forEach((e) {
+            if (e.name.toLowerCase().contains('adult')) {
+              filteredEvents.add(e);
+            }
+          });
+        }
+        break;
+
+      case 'public swim learner pool':
+        {
+          tmpEvents.forEach((e) {
+            if (e.name.toLowerCase().contains('public swim') &&
+                e.poolType == 'learner') {
+              filteredEvents.add(e);
+            }
+          });
+        }
+        break;
+
+      case 'bike':
+        {
+          tmpEvents.forEach((e) {
+            if (e.name.toLowerCase().contains('spin') ||
+                e.name.toLowerCase().contains('sprint') ||
+                e.name.toLowerCase().contains('trip')) {
+              filteredEvents.add(e);
+            }
+          });
+        }
+        break;
+
+      default:
+        {
+          filteredEvents = tmpEvents
+              .where(
+                  (e) => e.name.toLowerCase().contains(className.toLowerCase()))
+              .toList();
+        }
+    }
+    return filteredEvents;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:menu/menu.dart';
+
+import 'classes_all_days.dart';
+import 'classes_page.dart';
+import 'classes_timetable.dart';
+import 'event.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Timetable',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Timetable'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String classFilter = '';
+  String title = 'mdw';
+  String currentDay = 'monday';
+  LeisureCentre mdw = LeisureCentre();
+  int _currentPageIndex = 0;
+
+  var _value;
+  @override
+  Widget build(BuildContext context) {
+    //List tempList = mdw.classList(classFilter);
+
+    print('got tempList');
+    List myList;
+
+    final choices = List<Widget>.generate(mdw.classFilters.length, (i) {
+      var myClass = mdw.classFilters[i];
+      return ListTile(
+        title: Text(myClass),
+        onTap: () {
+          setState(
+                () {
+              classFilter = myClass;
+
+              title = 'MDW ${classFilter == "" ? 'all classes' : classFilter}';
+              _currentPageIndex=0;
+
+            },
+          );
+          Navigator.pop(context);
+        },
+      );
+    });
+
+//    final pages = List<Widget>.generate(7, (i) {
+//      myList = tempList.where((e) => e.day == mdw.days[i]).toList();
+//      return ClassesPage(
+//        myList: myList,
+//      );
+//    });
+//    final pagesAllDays = List<Widget>.generate(1, (i) {
+//      myList = tempList; //.where((e) => e.day == mdw.days[i]).toList();
+//      return ClassesAllDays(
+//        myList: myList,
+//      );
+//    });
+//
+//    final pagesClassesTimetable = List<Widget>.generate(1, (i) {
+//      myList = tempList.where((e) => e.type == 'pool').toList();
+//      return ClassesTimetable(
+//        width: MediaQuery.of(context).size.width,
+//        height: MediaQuery.of(context).size.height,
+//        myList: myList,
+//      );
+//    });
+
+    final List<Widget> pages2 = [
+      ClassesAllDays(
+        myList: mdw.classList(className: classFilter, filterDay: ''),
+      ),
+      ClassesTimetable(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        myList: mdw.classList(className:'', filterDay:'').where((e) => e.type == 'pool').toList(),
+      )
+    ];
+
+    return Scaffold(
+      backgroundColor: Colors.white70,
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      drawer: Drawer(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(children: choices),
+        ),
+      ),
+
+      body: pages2[_currentPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentPageIndex, // new
+        onTap: onTabTapped, // new
+// a new tab is tapped
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.accessibility_new),
+            title: new Text('Classes'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.beach_access,),
+            title: new Text('Swim timetable'),
+          ),
+//          BottomNavigationBarItem(
+//              icon: Icon(Icons.person), title: Text('Profile'))
+        ],
+      ),
+//      floatingActionButton: FloatingActionButton(
+//        onPressed: () {},
+//        tooltip: 'Increment',
+//        child: Icon(Icons.android),
+//      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentPageIndex = index;
+      if (index==0) {
+        title = 'MDW ${classFilter == "" ? 'all classes' : classFilter}';
+      }else{
+        title = 'MDW Swim';
+      }
+
+    });
+  }
+}
